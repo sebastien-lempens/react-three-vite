734d3dc76e8c7f37e066e85dffffea4567c6c715
telkeatelecom <telkeatelecom@gmail.com>
2024-03-07 10:07:52 +0100

    refactor@ refactor

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// This is required for Vite to work correctly with CodeSandbox
-const server = process.env.APP_ENV === "sandbox" ? { hmr: { clientPort: 443 } } : {};
-
 // https://vitejs.dev/config/
 export default defineConfig({
   server: server,
